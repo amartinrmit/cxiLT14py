9cefaaee0ea0b20c30da61ac895eed61ef9f8e3f
Andrew <andrew.martin@rmit.edu.au>
2018-08-16 09:25:21 +1000

    add standard deviation calculation to powder.py

--- powder.py
+++ powder.py
@@ -20,10 +20,13 @@ class powder:
 
 #        self.qgrids
 
-    def powderplot( self, qarr, data, nqbins, qmin=-1, qmax=-1): 
+    def powderplot( self, data, qarr, nqbins, qmin=-1, qmax=-1,\
+                    stdout=False): 
         
         # output arrays 
         pplot = np.zeros( nqbins )
+        if stdout==True:
+            splot = np.zeros( nqbins )
 
         # step size
         qstep = (qmax-qmin)/float(nqbins)
@@ -35,5 +38,31 @@ class powder:
             dq = qmin + i*qstep
             iq = np.logical_and( (qarr>dq), qarr<dq+qstep )
             pplot[i] = data.mean( iq )
+            if stdout==True:
+                splot[i] = data.std( iq )
 
-        return pplot
+        if stdout==True:
+            return pplot, splot
+        else:
+            return pplot
+
+    #
+    # histogram of pixel values within a certain q range
+    #
+    def qhistogram( self, data, qarr, qmin, qmax, nbins=100,
+                    dmin=-1e10, dmax=-1e10):
+
+        # get indices of pixels within the q range
+        iq = np.logical_and( (qarr>qmin), qarr<qmax )
+        if len(iq)==0:
+            print "powder.py; qhistogram(); no pixel found within qrange."
+            return [], []
+
+        if dmin == -1e10:
+            dmin = np.min( data[iq])
+        if dmax < -1e10:
+            dmax = np.max( data[iq])
+
+        h, b = np.histogram( data[iq], range=(dmin,dmax), nbins=nbins )
+
+        return h, b
