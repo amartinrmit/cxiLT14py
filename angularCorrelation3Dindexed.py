600cb59d607ca56ec9b7fa730652e6f729cd0841
Andrew Martin <andrewmartin@192-168-1-101.tpgi.com.au>
2021-04-22 20:32:02 +1000

    correction for q binning on pixel width

--- angularCorrelation3Dindexed.py
+++ angularCorrelation3Dindexed.py
@@ -90,7 +90,7 @@ cenx, ceny = atp.args.cenx, atp.args.ceny
 # set up qarrays
 #
 pixel_width = 109.8e-6
-qbins = ac.qbins( nq, atp.args.dz, atp.args.wl, pixel_width) 
+qbins = ac.qbins( nq, atp.args.dz, atp.args.wl, pixel_width*qmax/float(nq))  # correction for q binning on pixel width 
 
 
 
